e48761056e1dbbdb821250a4d799530a4487f818
Vipin MS <vipins2@vmware.com>
2022-11-03 16:00:04 +0530

    1667471400

--- hungryman-ui/src/git-update-wfd.sh
+++
@@ -1,8 +0,0 @@
-cd ~/where-for-dinner/
-commit=$(date +%s)
-echo $commit 
-git pull
-sed -i '' -E  "s/Submitted Searches And Results[0-9]+/Submitted Searches And Results$commit/g" ~/where-for-dinner/hungryman-ui/src/DiningSearches.js
-git add .
-git commit -m $commit
-git push
